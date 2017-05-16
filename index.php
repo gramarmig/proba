b72b4308ede78c717465a09b4d08423137c0bac2
gramarmig <somebody1121@gmail.com>
2017-05-16 20:23:31 +0200

    ENGADIMOS CSS

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php
-  phpinfo();
-?>
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+		<title>O MEU SITIO</title>
+		<?php
+			phpinfo();
+		?>
+		<link rel="stylesheet" href="style.css"/>
+	</head>
+	<body>
+	</body>
+</html>
